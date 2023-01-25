62dc08e3d8c15457b96cee45280f0faf77e81de1
Nicolas Pinto gomez <npinto-g@c2r15s2.42madrid.com>
2023-01-25 20:05:36 +0100

    Board, App - Referenced board's canvas node and added a font-family

--- srcs/frontend/src/App.vue
+++ srcs/frontend/src/App.vue
@@ -3,15 +3,24 @@ import Board from './components/Board.vue'
 </script>
 
 <template>
+<head>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Press+Start+2P"/>
+</head>
 <h1>TETRIS</h1>
-<Board/>	
+<Board />
 </template>
 
 <style>
+
 *
 {
 	margin: 0px;
 	padding: 0px;
 	box-sizing: border-box;
+}
+
+h1, p
+{
+	font-family: 'Press Start 2P', cursive;
 }
 </style>
--- srcs/frontend/src/components/Board.vue
+++ srcs/frontend/src/components/Board.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import	constants from './constants.ts';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 class Board {
 	cols: number;
 	rows: number;
 	width: number;
 	height: number;
+	canvas: HTMLCanvasElement;
 }
 
+
+let boardReference = ref(null);
 const board = new Board();
 
 board.cols = constants.COLS;
@@ -16,10 +19,23 @@ board.rows = constants.ROWS;
 board.width = constants.COLS * constants.BLOCK_SIZE;
 board.height = constants.ROWS * constants.BLOCK_SIZE;
 
+onMounted( () => {
+	board.canvas = boardReference.value.getContext('2d');
+	board.canvas.scale(constants.BLOCK_SIZE, constants.BLOCK_SIZE);
+});
+
 </script>
 
 <template>
-	<canvas class='board' :width="board.width" :height="board.height"></canvas>
+<div class="grid">
+	<canvas ref="boardReference" class="board" :width="board.width" :height="board.height"></canvas>
+	<div class="right-column">
+		<p>Score: <span id="score">0</span></p>
+		<p>Lines: <span id="lines">0</span></p>
+		<p>Level: <span id="level">0</span></p>
+		<canvas id="next" class="next"></canvas>
+	</div>
+</div>
 </template>
 
 <style scoped>
