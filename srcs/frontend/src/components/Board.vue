4c470d211bb215563f22f783ca13ae95f7dcbcef
Nicolas Pinto gomez <npinto-g@c3r4s1.42madrid.com>
2023-01-25 17:15:20 +0100

    Board - Initial canvas layout

---
+++ srcs/frontend/src/components/Board.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import	constants from './constants.ts';
+import { ref } from 'vue';
+
+class Board {
+	cols: number;
+	rows: number;
+	width: number;
+	height: number;
+}
+
+const board = new Board();
+
+board.cols = constants.COLS;
+board.rows = constants.ROWS;
+board.width = constants.COLS * constants.BLOCK_SIZE;
+board.height = constants.ROWS * constants.BLOCK_SIZE;
+
+</script>
+
+<template>
+	<canvas class='board' :width="board.width" :height="board.height"></canvas>
+</template>
+
+<style scoped>
+
+.board
+{
+	border: 3px solid red;/* for testing purposes */
+}
+
+</style>
---
+++ srcs/frontend/src/components/constants.ts
@@ -0,0 +1,9 @@
+const COLS: number = 10;
+const ROWS: number = 20;
+const BLOCK_SIZE: number = 30;
+
+export default {
+	COLS: COLS,
+	ROWS: ROWS,
+	BLOCK_SIZE: BLOCK_SIZE,
+}
